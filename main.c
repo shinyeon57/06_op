e8de047efe8025bb0c4182fd164805c0adbf36e1
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-10-15 16:09:55 +0900

    10/15 practice07

--- main.c
+++ main.c
@@ -4,19 +4,42 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	int main(void)	
+int factorial(int n)
 {
+	int res=1;
 	int i;
-	int max=45;
-	 
-	for(i=0;i<6;i++)
+	for(i=1;i<=n;i++)
 	{
-	printf ("%d", 1+rand()%max);
+		res = res * i;
 	}
+	
+	return res;
+}
+
+int calcCombination(int n, int r)
+{
+	int high, low;
+	high = factorial(n);
+	low = factorial(n-r)*factorial(r);
+	
+	return (high/low);
+	}
+
+
+int main(void)	{
+	
+	int n, r;
+	int res;
+	
+	printf("input n and r");
+	scanf("%d,%d", &n, &r);
+	
+	res = calcCombination(n,r);
+	printf("calcCombination result is %d\n", res);
+	
+	
 	return 0;
 }
 	
-
-
 
 
